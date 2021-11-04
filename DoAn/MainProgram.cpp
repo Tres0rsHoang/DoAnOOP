b6180c8157df7eca17ae78bac47fbd7ff3e27602
Hoàng Bảo <baokyo002@gmail.com>
2021-11-04 09:39:01 +0700

    First commit

---
+++ DoAn/MainProgram.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <Windows.h>
+#include <thread>
+#include <conio.h>
+#include <thread>
+#include "SetScreen.h"
+
+using namespace std;
+
+class Player {
+private:
+    int x;
+    int y;
+public:
+    Player() {
+        this->x = 0;
+        this->y = 0;
+    }
+    Player(screen PlayGround) {
+        int* background = PlayGround._getinform();
+        this->x = (background[2] - background[0]) / 2;
+        this->y = background[3] - 3;
+    }
+
+    void GotoXY(int x, int y) {
+        COORD coord;
+        coord.X = x;
+        coord.Y = y;
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+    }
+    void _show() {
+        GotoXY(this->x, this->y);
+        cout << "\\o/";
+        GotoXY(this->x, this->y + 1);
+        cout << " | ";
+        GotoXY(this->x, this->y + 2);
+        cout << "/ \\";
+        unsigned int milisecond = 1000;
+        Sleep(50);
+        GotoXY(this->x, this->y);
+        cout << " o ";
+        GotoXY(this->x, this->y + 1);
+        cout << "/|\\";
+        GotoXY(this->x, this->y + 2);
+        cout << "/ \\";
+    } 
+    void _destroy() {
+        GotoXY(this->x, this->y);
+        cout << "   ";
+        GotoXY(this->x, this->y + 1);
+        cout << "   " << endl;
+        GotoXY(this->x, this->y + 2);
+        cout << "   " << endl;
+    }
+    void _move(screen PlayGround){
+        int* background = PlayGround._getinform();
+        bool stop = false;
+       
+        while (!stop) {
+            char choose = toupper(_getch());
+            switch (choose) {
+            case 'W': {
+                if (this->y - 1 < background[3] && this->y - 1 > background[1]) {
+                    this->_destroy();
+                    this->y -= 1;
+                    this->_show();
+                }
+                break;
+            }
+            case 'S': {
+                if (this->y + 3 < background[3] && this->y + 3 > background[1]) {
+                    this->_destroy();
+                    this->y += 1;
+                    this->_show();
+                }
+                break;
+            }
+            case 'D': {
+                if (this->x + 3 < background[2] && this->x + 3 > background[0]) {
+                    this->_destroy();
+                    this->x += 1;
+                    this->_show();
+                }
+                break;
+            }
+            case 'A': {
+                if (this->x - 1 < background[2] && this->x - 1 > background[0]) {
+                    this->_destroy();
+                    this->x -= 1;
+                    this->_show();
+                }
+                break;
+            }
+            case 27: {
+                stop = true;
+                break;
+            }
+            default:
+                GotoXY(this->x + 3, this->y);
+                cout << "ERROR MOVING";
+                Sleep(800);
+                GotoXY(this->x + 3, this->y);
+                cout << "             ";
+            }
+        }
+        GotoXY(this->x + 3, this->y);
+        cout << "DONE!!!";
+        
+    }
+};
+
+int main() {
+    screen Menu(66, 0, 100, 40);
+    Menu._format();
+    Menu._printFrame(6);
+
+    screen PlayGround(0, 0, 66, 40);
+    PlayGround._printFrame(6);
+    
+    Player a(PlayGround);
+    a._show();
+
+    thread t1(&Player::_move, a, PlayGround);
+    t1.join();
+    
+    char delay = _getch();
+    return 0;
+}
---
+++ DoAn/SetScreen.h
@@ -0,0 +1,131 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+#include <cwchar>
+
+using namespace std;
+
+class screen {
+	int x, y;
+	int z, t;
+public:
+	screen(int x, int y, int z, int t) {
+		this->x = x;
+		this->y = y;
+		this->z = z;
+		this->t = t;
+	}
+	screen() {
+		this->x = this->y = this->z = this->t = 0;
+	}
+
+	//Chỉnh size màn hình
+	void SetWindowSize(SHORT width, SHORT height) {
+		HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+		SMALL_RECT WindowSize;
+		WindowSize.Top = 0;
+		WindowSize.Left = 0;
+		WindowSize.Right = width;
+		WindowSize.Bottom = height;
+		SetConsoleWindowInfo(hStdout, 1, &WindowSize);
+	}
+	//Khóa size màn hình
+	void FixConsoleWindow() {
+		HWND consoleWindow = GetConsoleWindow();
+		LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
+		style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
+		style &= ~(WS_MAXIMIZEBOX | WS_MINIMIZEBOX | WS_SIZEBOX); //tắt phòng to nhỏ
+		SetWindowLong(consoleWindow, GWL_STYLE, style);
+	}
+	//Di chuyển điểm trên màn hình
+	void GotoXY(int x, int y) {
+		COORD coord;
+		coord.X = x;
+		coord.Y = y;
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+	}
+	//Tắt tô chọn trong console
+	void DisableSelection() {
+		HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+		SetConsoleMode(hStdin, ~ENABLE_QUICK_EDIT_MODE);
+	}
+	//Chỉnh màu trong console
+	void SetColor(int backgound_color, int text_color) {
+		/*
+		0 = Black      8 = Gray
+		1 = Blue       9 = Light Blue
+		2 = Green      10 = Light Green
+		3 = Aqua       11 = Light Aqua
+		4 = Red        12 = Light Red
+		5 = Purple     13 = Light Purple
+		6 = Yellow     14 = Light Yellow
+		7 = White      15 = Bright White
+		*/
+		HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+		int color_code = backgound_color * 16 + text_color;
+		SetConsoleTextAttribute(hStdout, color_code);
+	}
+	//Chỉnh vị trí nhìn trong console
+	void SetScreenBufferSize(SHORT width, SHORT height) {
+		HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+		COORD NewSize;
+		NewSize.X = width;
+		NewSize.Y = height;
+		SetConsoleScreenBufferSize(hStdout, NewSize);
+	}
+	//Tắt con trỏ chuột trong console
+	void ShowCur(bool CursorVisibility) {
+		HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+		CONSOLE_CURSOR_INFO ConCurInf;
+
+		ConCurInf.dwSize = 10;
+		ConCurInf.bVisible = CursorVisibility;
+
+		SetConsoleCursorInfo(handle, &ConCurInf);
+	}
+
+	void _input(int x, int y, int z, int t) {
+		this->x = x;
+		this->y = y;
+		this->z = z;
+		this->t = t;
+	}
+	void _format() {
+		SetWindowSize(this->z, this->t);
+		SetScreenBufferSize(this->z + 1, this->t + 1);
+		FixConsoleWindow();
+		ShowCur(0);
+		//DisableSelection();
+	}
+	int* _getinform() {
+		int* result = new int[4];
+		result[0] = this->x;
+		result[1] = this->y;
+		result[2] = this->z;
+		result[3] = this->t;
+		return result;
+	}
+
+	//In khuôn trò chơi
+	void _printFrame(int color) {
+		//Vẽ khuôn
+		SetColor(0, color);
+		for (int i = this->y; i <= this->t;i++) {
+			GotoXY(this->x, i);
+			cout << (char)219;
+			GotoXY(this->z, i);
+			cout << (char)219;
+		}
+		for (int i = this->x;i <= this->z; i++) {
+			GotoXY(i, this->y);
+			cout << (char)219;
+			GotoXY(i, this->t);
+			cout << (char)219;
+		}
+		SetColor(0, 7);
+	}
+
+};
+
+
+
